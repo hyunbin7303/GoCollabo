c34aedb4b5787b44f8b4f10b4b2e9b323996cf6b
kevin <hyunbin7303@gmail.com>
2023-11-29 15:43:40 -0500

    Just Tidy and moduling testing

---
+++ Trex/go.mod
@@ -0,0 +1,3 @@
+module github.com/hyunbin7303/GoCollabo
+
+go 1.21.1
---
+++ Trex/trex.go
@@ -0,0 +1,9 @@
+package trex
+
+func Growling() string {
+	return "TREXXX"
+}
+
+func growling() string {
+	return "TREX TREX"
+}
